4cce1818a5980b4ca3b414aaf4fe0e54a021c355
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2025-07-26 05:31:48 -0400

    complete: project

--- index.js
+++ index.js
@@ -1,62 +1,98 @@
 const form = document.querySelector("form");
-const pBtns = document.querySelectorAll(".tip__btn");
-const peopleCount = document.querySelector("#peopleCount");
+const btns = document.querySelectorAll("button");
+const peopleCount = document.querySelector("#people");
 const error = document.querySelector(".error");
+const tipPerPerson = document.querySelector(".tip__per__person");
+const totalPerPerson = document.querySelector(".total__per__person");
+const customTip = document.querySelector(".tip__custom");
+const resetBtn = document.querySelector(".tip__details button");
+const billInput = document.querySelector("#bill");
+
 const validate = {
-  billAmount: (value) => value > 0,
-  tipPercent: (value) => value > 0,
-  tipPercent: (value) => value > 1,
+  bill: (value) => value > 0,
+  tip: (value) => value > 0,
+  people: (value) => value > 0,
 };
 
 const dataValidate = (key, value, validation) => {
   if (!validation[key]) return true;
   return validate[key](value);
 };
-
 const calculateTip = {
-  totalPerPerson: (bill, tip, people) =>
-    (bill + getPercent(bill, tip)) / people,
-  tipPerPerson: (bill, tip, people) => getPercent(bill, tip) / people,
+  totalPerPerson: ({ bill, tip, people }) =>
+    round((parseInt(bill) + getPercent(bill, tip)) / people),
+  tipPerPerson: ({ bill, tip, people }) =>
+    round(getPercent(bill, tip) / people),
 };
 
 const getPercent = (bill, tip) => {
   return (bill * tip) / 100;
 };
 
+const round = (num) => Math.round(num * 100) / 100;
 let clickedBtn = null;
 
-pBtns.forEach((btn) => {
+btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    clickedBtn = e.target;
+    btns.forEach((el) => el.classList.remove("active"));
+    if (e.target.id === "reset") {
+      console.log(btn);
+      form.reset();
+      totalPerPerson.textContent = "$0.00";
+      tipPerPerson.textContent = "$0.00";
+    } else {
+      clickedBtn = e.target;
+      clickedBtn.classList.add("active");
+      customTip.value = "";
+
+    }
   });
 });
-const formValid = (form, validation) => {
-  const formData = new FormData(form);
-  formData.append("tipPercent", clickedBtn.value);
-  const data = Object.fromEntries(formData);
-  let isValid = true;
-  Object.keys(data).forEach((name) => {
-    if (!dataValidate(name, data[name], validation)) isValid = false;
-  });
-  return isValid;
-};
+
 
 const handleClick = (e) => {
   e.preventDefault();
 
-  if (formValid(e.target, validate)) console.log("pass");
-  else console.log("fail");
+  if (billInput.value === "" && peopleCount.value === "" && clickedBtn === null)
+    resetBtn.disabled = true;
+  else {
+    resetBtn.disabled = false;
+  }
+    if (e.target.classList.contains('tip__custom'))  btns.forEach((el) => el.classList.remove("active"));
+
+  const formData = new FormData(form);
+
+  if (clickedBtn && formData.get("tip") === "")
+    formData.append("tip", clickedBtn.value);
+  const data = Object.fromEntries(formData);
+  let isValid = true;
+  Object.keys(data).forEach((name) => {
+    if (!dataValidate(name, data[name], validate)) isValid = false;
+  });
+
+  if (isValid) {
+    tipPerPerson.textContent = `$${calculateTip.tipPerPerson(data)}`;
+    totalPerPerson.textContent = `$${calculateTip.totalPerPerson(data)}`;
+  }
 };
 
 const handlePeopleCount = (e) => {
-  if (!parseInt(e.target.value) <= 0) {
-    error.innerHTML = " ";
+  const wrapper = e.target.closest(".input-wrapper");
+  if (parseInt(e.target.value) <= 0 || e.target === "") {
+    let err = e.target.closest(".form-group").querySelector(".error");
+    console.log(err);
+    err.textContent = "Can't be zero";
+    wrapper.classList.add("input-err");
     return;
   }
+  error.innerHTML = " ";
+  wrapper.classList.remove("input-err");
 
-  let err = e.target.closest("div")?.querySelector(".error");
-  err.textContent = "Can't be zero";
+  handleClick(e);
 };
-
+resetBtn.disabled = true;
+form.reset()
 peopleCount.addEventListener("input", handlePeopleCount);
 form.addEventListener("submit", handleClick);
+customTip.addEventListener("input", handleClick);
+billInput.addEventListener("input", handleClick);
